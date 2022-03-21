40f99f21dd9d0858ec45275a59f30b271dc1514c
omarwright07 <omarwright07@gmail.com>
2022-03-21 16:53:08 -0500

    create the sumbit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
 var buttonE1 = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#task-to-do");
+var formEl = document.querySelector("#task-form");
 
 var createTaskHandler = function() {
+
+    event.preventDefault();
+
     var taskItemEl = document.createElement("li");
     taskItemEl.className = "task-item";
     taskItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(taskItemEl);    
 }
 
-buttonE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
