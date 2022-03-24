61d7cb7581f2eacd911f27c00c98a0e19cc847dd
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-03-23 20:29:39 -0500

    added check to require you to fill out form

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@ var buttonE1 = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#task-to-do");
 var formEl = document.querySelector("#task-form");
 
-var taskFormHandler = function(event) {
+var taskFormHandler = function (event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    
+
     // package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -14,10 +14,16 @@ var taskFormHandler = function(event) {
     };
 
     // send it as an argument to createTaskEl
-    createTaskEl(taskDataObj);       
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
+    createTaskEl(taskDataObj);
 };
 
-var createTaskEl = function(taskDataObj) {
+var createTaskEl = function (taskDataObj) {
     // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
@@ -25,13 +31,13 @@ var createTaskEl = function(taskDataObj) {
     // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
- 
+
     // add HTML content to div
     taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
-    
+
     // add entire list item to list
-    tasksToDoEl.appendChild(listItemEl); 
+    tasksToDoEl.appendChild(listItemEl);
 }
 
 formEl.addEventListener("submit", taskFormHandler);
